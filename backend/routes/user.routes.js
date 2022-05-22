105228542c5df4e3619bcd44d2ada5802f550bc6
soumyagangamwar <soumyagangamwar@gmail.com>
2022-05-22 05:23:45 -0700

    users controllers created

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+const registerUser = require('../controllers/userControllers');
+
+router.route('/').post(registerUser)
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,22 @@ const express = require('express');
 const dotenv = require('dotenv');
 const notes = require('./data/notes');
 const connectDB = require('./config/db')
-
+const userRoutes = require('./routes/user.routes')
 const app = express();
 dotenv.config();
 connectDB();
+app.use(express.json())
+
+
 app.get('/', (req, res)=> {
       res.send('Welcome')
 })
 
-app.get('/notes', (req, res)=> {
+app.get('/api/notes', (req, res)=> {
       res.send(notes)
 })
 
-app.get('/notes/:id', (req, res)=> {
+app.get('/api/notes/:id', (req, res)=> {
       const note = notes.find(note=>note._id === req.params.id);
       if (note) {
             res.send(note);
@@ -22,6 +25,8 @@ app.get('/notes/:id', (req, res)=> {
             res.send('No Note Found.......')
       }
 })
+
+app.use('/api/users', userRoutes)
 
 const PORT = process.env.PORT || 5000;
 
